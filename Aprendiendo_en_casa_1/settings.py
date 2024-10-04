23fe2fc2f5fcdc26ab60adb6621baea87bc0db8c
Dilson <dilsonchimborazoperez@gmail.com>
2024-10-03 21:48:20 -0500

    Segunda api terminada y funcionando

--- Aprendiendo_en_casa_1/settings.py
+++ Aprendiendo_en_casa_1/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apps.PrimeraApi',
-    'apps.ApiLibros'
+    'apps.ApiLibros',
 ]
 
 MIDDLEWARE = [
--- apps/ApiLibros/api/serializers.py
+++ apps/ApiLibros/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from apps.ApiLibros.models import Libros
 
-class LibrosSerializers(Modelserializer):
+class LibrosSerializers(ModelSerializer):
     class Meta:
         model = Libros
         fields = '__all__'
